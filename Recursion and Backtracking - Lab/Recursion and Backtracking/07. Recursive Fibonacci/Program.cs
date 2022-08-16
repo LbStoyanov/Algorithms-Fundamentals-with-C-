dbe582bdce2b6c353342e9ad3f762cd478246d68
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-16 13:15:34 +0300

    RecursionAndBacktracking

--- Recursion and Backtracking - Lab/Recursion and Backtracking/07. Recursive Fibonacci/Program.cs
+++ Recursion and Backtracking - Lab/Recursion and Backtracking/07. Recursive Fibonacci/Program.cs
@@ -6,7 +6,18 @@ namespace _07._Recursive_Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateFibonacci(num));
+        }
+
+        private static long CalculateFibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+
+            return CalculateFibonacci(number - 1) + CalculateFibonacci(number - 2);
         }
     }
 }
