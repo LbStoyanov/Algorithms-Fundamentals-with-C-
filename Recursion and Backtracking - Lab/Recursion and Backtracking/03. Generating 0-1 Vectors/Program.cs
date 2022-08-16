7539b3da922fa2ae1af4fa52db2bf59573ad3517
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-16 11:02:14 +0300

    RecursionAndBackTracking

--- Recursion and Backtracking - Lab/Recursion and Backtracking/03. Generating 0-1 Vectors/Program.cs
+++ Recursion and Backtracking - Lab/Recursion and Backtracking/03. Generating 0-1 Vectors/Program.cs
@@ -6,7 +6,27 @@ namespace _03._Generating_0_1_Vectors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = int.Parse(Console.ReadLine()!);
+
+            var array = new int[n];
+
+            Generate01(array, 0);
+        }
+
+        private static void Generate01(int[] array, int index)
+        {
+
+            if (index >= array.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty,array));
+                return;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                array[index] = i;
+
+                Generate01(array,index + 1);
+            }
         }
     }
 }
