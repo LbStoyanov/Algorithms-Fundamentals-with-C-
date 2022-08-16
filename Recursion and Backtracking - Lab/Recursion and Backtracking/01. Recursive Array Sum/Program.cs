7c71e6f54e3242fd0d750731edbaf429b9f02cec
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-16 10:09:46 +0300

    RecursionAndBackTracking

---
+++ Recursion and Backtracking - Lab/Recursion and Backtracking/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(RecursiveArraySum(nums,0));
+        }
+
+        private static int RecursiveArraySum(int[] nums, int i)
+        {
+            if (i >= nums.Length)
+            {
+                return 0;
+            }
+
+            return nums[i] + RecursiveArraySum(nums, i + 1);
+        }
+    }
+}
